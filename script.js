d4e91c6fb1710b39b5bce6f0592d60ce323d8283
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-04-23 10:58:06 +0100

    name

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {}
+function padRow(name) {
+  return name;
+}
+const call = padRow();
+console.log(call);
 
 
 for (let i = 0; i < count; i = i + 1) {
